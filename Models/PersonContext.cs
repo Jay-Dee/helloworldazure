bb0e855e674c7f0211ec1c27690e75d07241e6de
Jay-Dee <johnsondesouza@gmail.com>
2021-03-03 20:52:18 +0000

    support for reading sqllite

--- Models/PersonContext.cs
+++ Models/PersonContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 namespace helloazure.Models {
     public class PersonContext : DbContext {
         public PersonContext(DbContextOptions<PersonContext> options) : base(options)   {
+            this.Database.EnsureCreated();
     }
 
     public DbSet<Person> Persons { get; set; }
--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,10 @@
+using helloazure.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
@@ -11,12 +13,16 @@ namespace helloazure
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) => this.Configuration = configuration;
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApiVersioning(opt => opt.ReportApiVersions = true);
-            services.AddDbContext<helloazure.Models.PersonContext>(opt => opt.UseInMemoryDatabase("Persons"));
+            services.AddDbContext<helloazure.Models.PersonContext>(opt => opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo
             {
