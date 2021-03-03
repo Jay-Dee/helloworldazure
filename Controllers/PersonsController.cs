033776d0cbb0474d9785613bff2ce0033c943255
Jay-Dee <johnsondesouza@gmail.com>
2021-03-03 17:58:25 +0000

    enabled console logging

--- Controllers/PersonsController.cs
+++ Controllers/PersonsController.cs
@@ -2,16 +2,18 @@ using System.Linq;
 using helloazure.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace helloazure.Controllers {
     public class PersonsController : ControllerBase{
         private readonly PersonContext _context;
+        private readonly ILogger<PersonsController> _logger;
 
-        public PersonsController(PersonContext context)
+        public PersonsController(PersonContext context, ILogger<PersonsController> logger)
         {
             _context = context;
-
-            if(!_context.Persons.Any())
+            _logger = logger;
+            if (!_context.Persons.Any())
             {
                 PersonGenerator.InitData(_context);
             }
@@ -23,8 +25,10 @@ namespace helloazure.Controllers {
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IQueryable<Person>> GetPersons([FromQuery] PersonsRequest request)
         {
+            _logger.LogInformation($"Processing GET request for extracting {request.Limit} items after {request.Offset}");
             var result = _context.Persons as IQueryable<Person>;
-            
+
+            _logger.LogInformation($"Successfully extracted {result.Count()} items in total");
             Response.Headers["x-total-count"] = result.Count().ToString();
 
             return Ok(result
